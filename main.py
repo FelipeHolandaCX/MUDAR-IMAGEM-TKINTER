e9bf56876c8965eb446daeabd668bf4f2d2d5b4f
FelipeHolandaCX <168671228+FelipeHolandaCX@users.noreply.github.com>
2024-07-14 19:48:30 -0300

    Update and rename PYTHON to main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ janela.title("AUTOMAÇÃO SIAVL INTERFACE ACCESS")
 janela.geometry("400x300")
 
 # Definir o ícone da janela com uma imagem existente na mesma pasta
-caminho_icone = r'C:\Users\PC GAMER\OneDrive\Área de Trabalho\automação\Projeto Selenium com Chrome - Webscraping meusdividendos\icone2.ico'
+caminho_icone = r'caminhodoarquivo'
 janela.iconbitmap(caminho_icone)  # Substitua "icone.ico" pelo nome da sua imagem
